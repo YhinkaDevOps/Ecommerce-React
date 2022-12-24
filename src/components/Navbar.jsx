02c9d0f0734af0ecf2560ad449872b4595b19e71
YhinkaDevOps <yinkabakare30@gnail.com>
2022-12-25 00:50:12 +0200

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react'
-import { BiBorderAll } from "react-icons/bi";
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-    const[MobileMenu, setMobileMenu ] = useState(true);
-    const handleClick = () => setMobileMenu(!MobileMenu);
+  const [MobileMenu, setMobileMenu] = useState(false);
   return (
     <div>
-        <header className='header'>
-            <div className="container d_flex">
-                <div className="categories d_flex">
-                    <span className=''><BiBorderAll/></span>
-                    <h4>Categories <i class="fa-solid fa-caret-down"></i></h4>
-                </div>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="categories d_flex">
+            <span>
+              <i className="fa-solid fa-border-all"></i>
+            </span>
+            <h4>
+              Categories <i className="fa-solid fa-caret-down"></i>
+            </h4>
+          </div>
 
-                <div className='navlink'>
-                    <ul className={MobileMenu ? 'nav-links-MobileMenu' : "link f_flex capitalize"} onClick={handleClick}>
-                        <li><Link to='/'>Home</Link></li>
-                         <li><Link to='/pages'>Pages</Link></li>
-                         <li><Link to='/vendor'>User account</Link></li>
-                         <li><Link to='/track'>My order</Link></li>
-                         <li><Link to='/contact'>Contact</Link></li>
-                    </ul>
+          <div className="navlink">
+            <ul
+              className={
+                MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+              }
+              onClick={() => setMobileMenu(false)}
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pages">Pages</Link>
+              </li>
+              <li>
+                <Link to="/vendor">User account</Link>
+              </li>
+              <li>
+                <Link to="/track">My order</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
 
-                    <button className='toggle' onClick={handleClick}>
-                        {MobileMenu ? <i class="fa-solid fa-times close home-bth"></i>:
-                        <i class="fa-solid fa-xmark open"></i>
-                        }
-                    </button>
-                </div>
-            </div>
-        </header>
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!MobileMenu)}
+            >
+              {MobileMenu ? (
+                <i className="fa-solid fa-xmark"></i>
+              ) : (
+                <i className="fa-solid fa-bars"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../components/images/logo4.png'
+import logo from '../assets/img/olayinka.jpg'
 import {Link} from 'react-router-dom';
 
 const Search = () => {
@@ -9,29 +9,30 @@ const Search = () => {
   })
   return (
     <div>
-      <section className='search'>
+      <section className="search">
         <div className="container c_flex">
-          <div className='logo width'>
-            <img src={logo} alt="" />
+          <div className="logo width">
+            <img className="logo__" src={logo} alt="logo" />
           </div>
-          <div className='search-box f_flex'>
-           <i class="fa fa-search"></i>
-            <input type="text"  placeholder='Search and hit enter...' />
+          <div className="search-box f_flex">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search and hit enter..." />
             <span>All Category</span>
           </div>
 
           <div className="icon f_flex width">
-           <i class="fa-solid fa-user"></i>
-            <div className='cart'>
-              <Link to ="/cart"><i class="fa-solid fa-cart-shopping"></i></Link>
+            <i className="fa-solid fa-user icon-circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa-solid fa-cart-shopping icon-circle"></i>
+              </Link>
               <span>0</span>
             </div>
           </div>
-
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default Search
